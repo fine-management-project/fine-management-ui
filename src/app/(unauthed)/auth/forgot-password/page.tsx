64ce699ba2716d88d0811917b615ba9bb02cc23a
Yevhenii Shcherbyna <scherbinag@ukr.net>
2026-01-23 19:08:15 +0200

    feature: fixed build and added suspense

--- src/app/(unauthed)/auth/forgot-password/page.tsx
+++ src/app/(unauthed)/auth/forgot-password/page.tsx
@@ -1,7 +1,12 @@
 import ChangePasswordForm from "@/components/ForgotPasswordPage/ChangePasswordForm";
+import { Suspense } from "react";
 
 const ForgotPasswordPage = (): React.JSX.Element => {
-  return <ChangePasswordForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ChangePasswordForm />
+    </Suspense>
+  );
 };
 
 export default ForgotPasswordPage;
