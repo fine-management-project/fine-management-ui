373015b163427c53f6c125de7e3f1192fa0ab779
Yevhenii Shcherbyna <64918675+creogenus@users.noreply.github.com>
2026-01-23 19:10:14 +0200

    feature: integrate GitHub Actions and tests (#12)

--- src/app/(authed)/change-email/page.tsx
+++ src/app/(authed)/change-email/page.tsx
@@ -1,29 +1,32 @@
 import ChangeEmailForm from "@/components/ChangeEmailPage/ChangeEmailForm";
 import { Button } from "@/components/ui/button";
 import { createServerSession } from "@/lib/session/server";
+import { Suspense } from "react";
 
 const ChangeEmailPage = async (): Promise<React.JSX.Element> => {
   const session = await createServerSession();
 
   return (
-    <div className="h-96 text-center content-center">
-      <ChangeEmailForm />
-      <h2 className="text-2xl font-bold">
-        If you do not want to change the email, please, proceed back to other
-        pages.
-      </h2>
-      <div className="flex justify-center gap-8 mt-8">
-        <Button variant="link" className="text-4xl p-0 text-purple-800">
-          <a href="/dashboard">Dashboard</a>
-        </Button>
-
-        {session.hasUserId() && (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="h-96 text-center content-center">
+        <ChangeEmailForm />
+        <h2 className="text-2xl font-bold">
+          If you do not want to change the email, please, proceed back to other
+          pages.
+        </h2>
+        <div className="flex justify-center gap-8 mt-8">
           <Button variant="link" className="text-4xl p-0 text-purple-800">
-            <a href={`/users/${session.getUserId()}`}>Profile</a>
+            <a href="/dashboard">Dashboard</a>
           </Button>
-        )}
+
+          {session.hasUserId() && (
+            <Button variant="link" className="text-4xl p-0 text-purple-800">
+              <a href={`/users/${session.getUserId()}`}>Profile</a>
+            </Button>
+          )}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
